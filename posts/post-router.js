5954653daa24db33b4881b9895858c5859e85929
Psage23 <pamelasage3@gmail.com>
2020-01-28 21:35:04 -0700

    completed project

--- posts/post-router.js
+++ posts/post-router.js
@@ -5,24 +5,53 @@ const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const posts = await db('posts');
+        res.json(posts)
+    }
+    catch (err) {
+        res.status(500).json({message: "failed to get posts"});
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const [post] = await db('posts').where('id', req.params.id);
+        res.json(post);
+    } catch(err) {
+        res.status(500).json({message: "Failed to get id"})
+    }
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const postData = req.body;
+    try {
+        const post = await db('posts').insert(postData);
+        res.json(post)
+    } catch (err) {
+        res.status(500).json({message: "Failed to post"})
+    }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const rowsUpdated = await db('posts').where('id', id).update(req.body);
+        res.status(200).json({updated: rowsUpdated})
+    } catch (err) {
+        res.status(500).json({message: "Failed to update"})
+    }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const rowsDeleted = await db('posts').del('posts').where('id', req.params.id)
+        res.json({deletedRecords: rowsDeleted})
+    } catch (err) {
+        res.status(500).json({message: "Failed to delete"})
+    }
 });
 
 module.exports = router;
